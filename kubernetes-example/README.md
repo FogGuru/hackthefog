c3c8fe5444f799afe27abedc5d450fcaae3f2932
moule3053 <mulugetayalew@gmail.com>
2021-03-22 08:25:06 +0100

    Example code and instructions

--- kubernetes-example/README.md
+++ kubernetes-example/README.md
@@ -1,1 +1,27 @@
 # Kubernetes example for Hack the Fog hackathon
+
+In this example, we show how you can containerize a simple flask app that reads and displays the water temperature data from quality sensor. It subscribes to the `smart_water/49` MQTT topic and outputs the temperature data on a web interface.
+
+## Build the Docker image and push to an image repository (Docker Hub)
+
+In this example, we show building the image and pushing to a dockerhub repository.
+
+If you are using Docker Hub, please make sure that you have created an account on https://hub.docker.com/ and that you have installed Docker on your computer.
+
+Log in to your Docker Hub account by using the command
+
+```
+docker login
+```
+
+You need to build a Docker image that is compatible with the arm7 architecture.
+
+```
+docker buildx build --platform linux/arm/v7 -t REPOSITORY_NAME/IMAGE_NAME --push .
+```
+
+## Push the Docker image into an image repository (Dockerhub)
+
+## Deploy the application on Kubernetes
+
+## Access the application
---
+++ kubernetes-example/app/main.py
@@ -0,0 +1,84 @@
+from flask import Flask, request, redirect, url_for, abort,  json
+import paho.mqtt.client as mqtt
+import datetime
+import json
+
+default_port=1883
+broker_address = "192.168.9.20"
+mqtt_topic = 'smart_water/49'
+mqtt_user = 'participant'
+mqtt_password = 'participant'
+
+
+app = Flask(__name__)
+
+subscribed = False
+
+@app.route('/')
+def home():
+    html = "<html>" \
+           "<head><title>Welcome to La Marina de Valencia</title></head>" \
+           "<body><h2>Welcome to La Marina de Valencia</h2>" \
+           "<body><h3>Current sea water conditions</h3>"
+
+    global subscribed
+    if subscribed == False:
+        subscribe_on_topic()
+    subscribed = True
+
+    list_lines = str(read_message()).split('\n')
+    #html = ''
+    for line in list_lines:
+        if line != '':
+            html += "<p>Temperature: " + str(line) + "</p>"
+    #return html.format()
+    return html
+
+@app.route('/subscribe')
+def subscribe():
+    global subscribed
+    if subscribed == False:
+        subscribe_on_topic()
+    subscribed = True
+    html = "<b>Subscribed</b>"
+    return html.format()
+
+def read_message():
+    try:
+        f = open("messages.log", "r+")
+        message = f.read()
+        f.close()
+    except Exception as e:
+        message = "No messages found"
+    return message
+
+def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
+    print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
+    client.subscribe(mqtt_topic)  # Subscribe to the topic “digitest/test1”, receive any messages published on it
+
+
+def on_message(client, userdata, message):
+    print("time ", datetime.datetime.now())
+    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message topic=",message.topic)
+    #formated_message = str(datetime.datetime.now()) + "- Received message: "+ str(message.payload.decode("utf-8")) + ", on topic: " + str(message.topic) + "\n"
+    formated_message = str(message.payload.decode("utf-8"))
+    res = json.loads(formated_message)
+    print(res)
+    f = open("messages.log", "w")
+    f.write(str(res['SensorData']['WaterTemperature']))
+    f.close()
+
+def subscribe_on_topic(topic = mqtt_topic):
+    client = mqtt.Client("subscriber7")
+    client.on_message = on_message  # attach function to callback
+    client.on_connect = on_connect  # attach function to callback
+    print("connecting to broker: " + broker_address)
+    client.connect(broker_address, port=default_port) #connect to broker
+    client.username_pw_set(username=mqtt_user, password=mqtt_password)
+    client.loop_start()  # start the loop
+    print("Subscribing to topic: " + mqtt_topic)
+    client.subscribe(mqtt_topic)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
